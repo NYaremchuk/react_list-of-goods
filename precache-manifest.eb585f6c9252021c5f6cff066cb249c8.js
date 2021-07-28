e63a9379112617eb79e79ca2a2df20c6bcfdeed8
NYaremchuk <nazarmails@gmail.com>
2021-07-28 18:35:10 +0300

    Updates

---
+++ precache-manifest.eb585f6c9252021c5f6cff066cb249c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6971f0e5d9a5d70dbd35466a885f5305",
+    "url": "./index.html"
+  },
+  {
+    "revision": "99f839ad68aa6360ee16",
+    "url": "./static/css/main.68fc62ab.chunk.css"
+  },
+  {
+    "revision": "0dfb5bb716152e24f682",
+    "url": "./static/js/2.dc21eb97.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dc21eb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f839ad68aa6360ee16",
+    "url": "./static/js/main.ec07c22e.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
